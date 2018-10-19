96188061799e11004f4feeb0016c56fb1b191bd7
Lam Ho <hotienlam@gmail.com>
2018-10-19 19:45:29 +0700

    [UserManagement] Render sample app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import renderSampleApp from './sample/index.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+renderSampleApp();
