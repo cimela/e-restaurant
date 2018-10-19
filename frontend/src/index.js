a9d1a7009bbd5f135ceca098b84aa7d50b0b1a6c
Lam Ho <hotienlam@gmail.com>
2018-10-19 21:00:29 +0700

    [UserManagement] Create custom layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+// import App from './components/App';
+import CustomLayout from './components/Layout';
 import registerServiceWorker from './registerServiceWorker';
 import renderSampleApp from './sample/index.js';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<CustomLayout />, document.getElementById('root'));
+registerServiceWorker();
 
-renderSampleApp();
+// renderSampleApp();
